cb6efdc9280d17c71acbc08c15baaab79de64d88
Tomasz Kałędkowski <tomek113344@gmail.com>
2019-10-17 17:31:48 +0200

    'p'

--- funkcja_t.py
+++ funkcja_t.py
@@ -1,8 +1,5 @@
 def funckja_t(text="", letter=''):
-    for i in range(len(text)):
-        if text[i]==letter:
-            text[i]=' '
-    return text
+    return text.replace(letter, '')
 
 
 print(funckja_t(text="ala ma kota", letter='a'))
